db0fced7b1a455fa820547ac9bbb3bce08c6789a
Bilawal Khan <kbilawal84437@gmail.com>
2024-08-05 17:52:19 +0500

    Configure Redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from './appwrite/auth.js'
+import { login, logout } from './store/authSlice.js'
+import { Header, Footer } from './components/index.js'
+import { Outlet } from "react-router-dom"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  console.log("Process: ", import.meta.env.VITE_APP_APPWRITE_URL)
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then(userData => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO:
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 
   return (
     < div >
